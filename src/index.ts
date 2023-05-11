552f4f3d06d1bf874ae21479b738daa4b418705d
iamajraj <akmalraj07@gmail.com>
2023-05-11 20:20:03 +0600

    functions added to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { chunkArray } from "./chunkArray";
 export { deepClone } from "./deepClone";
 export { deepMerge } from "./deepMerge";
 export { factorial } from "./factorial";
+export { groupBy } from "./groupBy";
+export { flattenObject } from "./flattenObject";
